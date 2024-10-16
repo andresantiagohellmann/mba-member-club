ed0100b8401fe10e8146b436fe29417b180945c7
Trydo Studio <trydostudio@gmail.com>
2024-10-16 16:44:00 -0300

    add styles to sections and page-content

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,10 @@ import './styles/reset.css'
 import './styles/variables.css'
 import './styles/globals.css'
 import './styles/page-header.css'
-import './styles/page-aside.css'
 import './styles/page-content.css'
+import './styles/section-profile.css'
+import './styles/section-history.css'
+import './styles/section-stamps.css'
+import './styles/section-progress.css'
 
 // Scripts
